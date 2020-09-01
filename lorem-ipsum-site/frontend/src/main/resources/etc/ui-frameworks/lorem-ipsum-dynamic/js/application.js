35bf2541781993d1ffce9cc08d5c09e07ffba9a1
Danny Moulton <danny.moulton@kestros.io>
2020-09-01 15:20:39 -0400

    removed kestros-dynamic-content usage

--- lorem-ipsum-site/frontend/src/main/resources/etc/ui-frameworks/lorem-ipsum-dynamic/js/application.js
+++
@@ -1,5 +0,0 @@
-let application = new DynamicApplication(document.body)
-application.registerInteractiveElementType('.dynamic-content',
-    DynamicContentArea);
-application.initializeInteractiveElements(application.element)
-
