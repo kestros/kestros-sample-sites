a9e9d7aeb23254d1a8d89ced2cebd2479a7e2c1f
Danny Moulton <danny.moulton@kestros.io>
2021-05-29 20:47:28 -0400

    updated explore site content

---
+++ explore/app/src/main/resources/etc/ui-frameworks/explore-framework/versions/2.1.0/js/rellax-impl.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new Rellax('.rellax-slow', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    zindex: -1
+  });
+
+  new Rellax('.rellax-slower', {
+    speed: -7,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    zindex: -1
+  });
+
+  new Rellax('.rellax-slowest', {
+    speed: -10,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    zindex: -1
+  });
+
+  new Rellax('.rellax-fast', {
+    speed: 5,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    zindex: -1
+  });
+  new Rellax('.rellax-faster', {
+    speed: 7,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    zindex: -1
+  });
+  new Rellax('.rellax-fastest', {
+    speed: 10,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    zindex: -1
+  });
+})
+
