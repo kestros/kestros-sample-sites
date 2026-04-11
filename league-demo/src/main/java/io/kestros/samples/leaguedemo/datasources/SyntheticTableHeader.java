697259bcf571779974010ca0ba0b2cfb304020be
Danny Moulton <danny.moulton@kestros.io>
2026-04-11 00:40:15 -0400

    feat: add league-demo sample site module with player game log datasource
    New module for league-demo datasources following the signal-conf pattern.
    Includes PlayerGameLogTableDataSource that reads match data from
    LeagueDataService and renders as a KestrosTable.
    
    Also adds synthetic table helper classes (SyntheticTableCell,
    SyntheticTableHeader, SyntheticTableRow) matching signal-conf.

---
+++ league-demo/src/main/java/io/kestros/samples/leaguedemo/datasources/SyntheticTableHeader.java
@@ -0,0 +1,27 @@
+package io.kestros.samples.leaguedemo.datasources;
+
+import io.kestros.cms.components.basic.api.exceptions.ComponentConfigurationException;
+import io.kestros.cms.components.basic.api.table.KestrosTableHeader;
+import io.kestros.cms.components.basic.core.BaseSlingModelDataSource;
+import io.kestros.cms.components.basic.core.BaseSyntheticResource;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class SyntheticTableHeader extends BaseSyntheticResource implements KestrosTableHeader {
+
+  private final String text;
+
+  public SyntheticTableHeader(@Nonnull String text,
+      @Nonnull BaseSlingModelDataSource dataSource,
+      @Nonnull String resourcePrefix,
+      @Nullable String forcedResourceName) throws ComponentConfigurationException {
+    super(dataSource, resourcePrefix, forcedResourceName);
+    this.text = text;
+  }
+
+  @Nullable
+  @Override
+  public String getText() {
+    return text;
+  }
+}
