52ba1a5f6a699dfd5b1bc2745a8db4f255118171
Danny Moulton <ddmoultonvwc@gmail.com>
2026-08-08 05:22:41 -0400

    chore: drop the import orphaned by deleting goodClub()
    HashMap was used only by that helper. Left behind by the previous
    commit; no checkstyle is bound in this module to catch it.
    
    ..

--- acpl/application/src/test/java/io/kestros/samples/acpl/application/datasources/ClubDirectoryCardListDataSourceTest.java
+++ acpl/application/src/test/java/io/kestros/samples/acpl/application/datasources/ClubDirectoryCardListDataSourceTest.java
@@ -18,7 +18,6 @@ import io.kestros.cms.componenttypes.api.services.ComponentVariationRetrievalSer
 import io.kestros.samples.acpl.api.services.TeamService;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.sling.api.resource.Resource;
